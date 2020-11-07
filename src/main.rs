6527a3aef0c19a597dab66d4176422566df076d3
Naveen Mathew <naveenmat98@gmail.com>
2020-11-07 13:43:54 +1300

    Move logic to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
+use minigrep::Config;
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
 fn main() {
@@ -18,31 +17,9 @@ fn main() {
         config.query, config.filename
     );
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("{}", e);
 
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    Ok(())
-}
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Please provide a query and a filename");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
