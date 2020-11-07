6b18980c618dfe7dc4321b5cc492a60db92105fd
Naveen Mathew <naveenmat98@gmail.com>
2020-11-07 13:33:12 +1300

    Handle erros and parse arguments properly

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // Take arguments from the command line
     let args: Vec<String> = env::args().collect();
 
     // Store a reference to the query and filename from the arguments passed
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("{}", err);
+        process::exit(1);
+    });
+
+    println!(
+        "Searching for `{}` in file `{}`",
+        config.query, config.filename
+    );
 
     // Read the file
-    let contents = fs::read_to_string(filename)
-        .expect(&format!("Something went wrong while reading `{}`", filename)[..]);
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong while reading the file");
+}
 
-    println!("Searching for `{}` in file `{}`", query, filename);
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Please provide a query and a filename");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
