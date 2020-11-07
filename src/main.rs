3dc4bfb8499c5b9bede032453862f5d64804fd43
Naveen Mathew <naveenmat98@gmail.com>
2020-11-07 16:47:30 +1300

    Use iterators

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,8 @@ use std::env;
 use std::process;
 
 fn main() {
-    // Take arguments from the command line
-    let args: Vec<String> = env::args().collect();
-
-    // Store a reference to the query and filename from the arguments passed
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    // Take arguments and store the specified configuration
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("{}", err);
         process::exit(1);
     });
