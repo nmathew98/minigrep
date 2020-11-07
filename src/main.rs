58d13095e93538237196763f7f1bc8352aab4eb8
Naveen Mathew <naveenmat98@gmail.com>
2020-11-07 13:04:46 +1300

    Take arguments from command line

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
 
 fn main() {
+    // Take arguments from the command line
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    // Store a reference to the query and filename from the arguments passed
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for `{}` in file `{}`", query, filename);
 }
